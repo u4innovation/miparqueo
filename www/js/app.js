f824a36351108cd733abe51c660363f4545f780b
cvramire <cvramirezespinoza@gmail.com>
2016-08-18 08:55:47 -0500

    Cambios de estilo de aplicacion
    se cambió las imágenes de icono y splash de la aplicación, color en el
    css de background de pantallas, y se agregó la comparación de internet

--- www/js/app.js
+++ www/js/app.js
@@ -7,21 +7,37 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
-.run(function($ionicPlatform, $rootScope, AccountService) {
+.run(function($ionicPlatform, $rootScope, AccountService, $ionicPopup) {
 
   $ionicPlatform.ready(function() {
+                       
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
-
     }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
-  });
+    //Revisar la conexion a internet
+    if(window.Connection) {
+        if(navigator.connection.type == Connection.NONE) {
+        $ionicPopup.confirm({
+                           title: "Internet Disconnected",
+                           content: "The internet is disconnected on your device."
+                           })
+        .then(function(result) {
+             if(!result) {
+             ionic.Platform.exitApp();
+             }
+             });
+        }
+    }
+
+                       
+});
 
   AccountService.currentUser()
     .then(function(user) {
