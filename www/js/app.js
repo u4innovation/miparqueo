310321a53167da62b2f719858df8b1bfb13d39ce
gunsha <gonzaller@gmail.com>
2016-08-21 16:46:11 -0300

    mapa principal

--- www/js/app.js
+++ www/js/app.js
@@ -60,7 +60,7 @@ angular.module('starter', ['ionic','ngCordova', 'starter.controllers', 'starter.
     }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/home');
+  $urlRouterProvider.otherwise('/app/mapa');
 })
 .directive('map', function() {
   return {
