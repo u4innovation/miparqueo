108b6248ecf75dcb8ea82fd64f7830e78a272d39
Gonzalo <gonzaller@gmail.com>
2016-11-14 16:08:41 -0300

    mapa

--- www/js/mapaCtrl.js
+++ www/js/mapaCtrl.js
@@ -240,7 +240,8 @@ function MapaCtrl($ionicLoading, $ionicModal, s, r, $cordovaGeolocation, $http, 
         var mapOptions = {
             zoom: 16,
             mapTypeId: google.maps.MapTypeId.ROADMAP,
-            disableDefaultUI: true
+            disableDefaultUI: true,
+            styles: [{"featureType":"all","stylers":[{"saturation":0},{"hue":"#e7ecf0"}]},{"featureType":"road","stylers":[{"saturation":-70}]},{"featureType":"transit","stylers":[{"visibility":"off"}]},{"featureType":"poi","stylers":[{"visibility":"off"}]},{"featureType":"water","stylers":[{"visibility":"simplified"},{"saturation":-60}]}]
         };
         s.map = new google.maps.Map(document.getElementById('mapa'), mapOptions);
         s.centrarMapa();
